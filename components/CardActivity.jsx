25ae431ab16d3a7514c450ace49259562e3948e7
Kristian Bønløkke <krbonlokke@gmail.com>
2025-01-30 15:20:42 +0100

    add images to public folder

--- components/CardActivity.jsx
+++ components/CardActivity.jsx
@@ -6,7 +6,7 @@ export default function CardActivity({ activity }) {
         <Link href={`/activities/${activity.id}`}>
             <article className="rounded-l-3xl rounded-tr-3xl grid grid-cols-1 grid-rows-3 h-[320px]">
                 <Image
-                    src={activity.asset.url}
+                    src={`/${activity.id}.jpg`}
                     width={1000} height={1000} alt={activity.name}
                     className="rounded-l-3xl rounded-tr-3xl col-start-1 row-start-1 row-span-3 z-[-10] h-full object-cover"
                     priority
