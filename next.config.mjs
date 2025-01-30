8fcc145bfc3d8015d4cd84aa5ea937e1f0b13701
Kristian Bønløkke <krbonlokke@gmail.com>
2025-01-30 14:38:44 +0100

    change next.config images domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "localhost",
-                port: "4000",
-                pathname: "/**",
-              },
-              {
-                protocol: "https",
-                hostname: "kb-landrup-dans-api.onrender.com",
-                port: "10000",
-                pathname: "/**",
-              },
+        domains: [
+          'localhost',
+          'kb-landrup-dans-api.onrender.com'
+            
         ]
     }
 };
